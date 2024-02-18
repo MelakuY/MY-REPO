a9e669f11330eedc9dba9c4de810fdcb0d76b0d1
MelakuY <melyosak@gmail.com>
2024-02-18 11:21:51 +0300

    first commit for my web page project

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onclick = () => {
+  navigation.classList.toggle('open');
+}
+
+const listItems = document.querySelectorAll('.list-item');
+listItems.forEach(item => {
+  item.onclick = () => {
+    listItems.forEach(item => item.classList.remove('active'));
+    item.classList.add('active');
+  }
+})
+
+$( ".inner-switch" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "OFF" );
+    } else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "ON" );
+    }
+  });
